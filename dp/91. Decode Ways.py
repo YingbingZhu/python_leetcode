b57a0dcb071fed5b26c574ad8716ce2242801a12
YingbingZhu <yingbing.zhu@hotmail.com>
2022-10-01 20:04:13 +0800

    feat: 91

---
+++ dp/91. Decode Ways.py
@@ -0,0 +1,43 @@
+# A message containing letters from A-Z can be encoded into numbers using the following mapping:
+#
+# 'A' -> "1"
+# 'B' -> "2"
+# ...
+# 'Z' -> "26"
+# To decode an encoded message,
+# all the digits must be grouped then mapped back into letters
+# using the reverse of the mapping above (there may be multiple ways).
+# For example, "11106" can be mapped into:
+#
+# "AAJF" with the grouping (1 1 10 6)
+# "KJF" with the grouping (11 10 6)
+# Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+#
+# Given a string s containing only digits, return the number of ways to decode it.
+#
+# The test cases are generated so that the answer fits in a 32-bit integer.
+
+class Solution(object):
+    def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if not s or s[0] == '0':
+            return 0
+        # dp[i] store result for s with length i-1
+        dp = [0] * (len(s) + 1)
+
+        # base case
+        dp[0] = dp[1] = 1
+
+        for i in range(2, len(s) + 1):
+            # if its not 0
+            if s[i-1:i] != '0':
+                dp[i] = dp[i-1]
+            if 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+        return dp[len(s)]
+
+
+
