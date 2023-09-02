69f90d6bbecb545e106e272fce7edaf588057b0c
YingbingZhu <yingbing.zhu@hotmail.com>
2023-09-02 23:33:58 +0800

    feat:problems

---
+++ tree/106. Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not inorder or not postorder:
+            return None
+        r = postorder.pop()
+        root = TreeNode(r)
+        r_index = inorder.index(r)
+        root.right = self.buildTree(inorder[r_index + 1:], postorder)
+        root.left = self.buildTree(inorder[:r_index], postorder)
+        return root
