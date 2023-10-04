f2840032f9eafe2fc60066193dd78748f50f4477
YingbingZhu <yingbing.zhu@hotmail.com>
2023-10-04 22:35:45 +0800

    feat:problems

---
+++ linked_list/148. Sort List.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        mid = slow.next
+        slow.next = None
+
+        left = self.sortList(head)
+        right = self.sortList(mid)
+
+        # merge
+        dummy = ListNode(0)
+        p = dummy
+        while left and right:
+            if left.val < right.val:
+                p.next = left
+                left = left.next
+            else:
+                p.next = right
+                right = right.next
+            p = p.next
+        if left:
+            p.next = left
+        if right:
+            p.next = right
+        return dummy.next
