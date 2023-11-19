a7dbf7b12ac7ea997be6325943f26f26ff5716a2
YingbingZhu <yingbing.zhu@hotmail.com>
2023-11-19 21:43:23 +0800

    weekly contest

---
+++ maths/2938. Separate Black and White Balls.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        ans = 0
+        # l : how many ones seen
+        l = 0
+        for i in range(len(s)):
+            if s[i] == '0':
+                ans += i - l
+                l += 1
+        return ans
+        
